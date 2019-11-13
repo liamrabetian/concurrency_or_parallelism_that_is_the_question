4157ab99693d516f0630211446a973726edb0a39
mohammadrabetian <tommyrabetian@gmail.com>
2019-11-13 16:35:12 +0330

    sync version of our program

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # concurrency_or_parallelism_that_is_the_question
+
+In this project we will see the difference between concurrency,
+parallelism ... sync, async, threading, multiprocessing
---
+++ sync.py
@@ -0,0 +1,25 @@
+import time
+import requests
+
+
+def download_site(url, session):
+    with session.get(url) as response:
+        print(f'read {len(response.content)} from {url}')
+
+
+def download_all_sites(sites):
+    with requests.Session() as session:
+        for url in sites:
+            download_site(url, session)
+
+
+if __name__ == '__main__':
+    sites = [
+        'https://www.jython.org',
+        'https://www.python.org',
+    ] * 80
+
+start_time = time.time()
+download_all_sites(sites)
+duration = time.time() - start_time
+print(f'Downloaded {len(sites)} in {duration} seconds!')
